1cfb1971447911e32d5d4e39de2d838af7788ee5
Mariz Melo <mm@xchema.com>
2013-01-25 12:36:51 -0800

    posts, home button

--- xtyle/js/marizmelo.js
+++ xtyle/js/marizmelo.js
@@ -2,6 +2,12 @@
 define(["jquery"], function($){
   console.log("marizmelo.js loaded");
 
+  // FIRST TIME OPENING PAGE
+  $('aside img').load(function(){
+    $("#marizlogo").css( { "margin-left" : "80px", "margin-top" : "100px" }).delay(1000).animate( { "margin-left" : "0", "margin-top" : "0" } ); 
+  });
+
+  // MOUSE OVER LOGO EFFECT
   $("#marizimage").on("mouseenter", function () {
     $("#marizlogo").stop().animate( { "margin-left" : "80px", "margin-top" : "100px" }, function() {
       $(this).on("mouseleave", function(){ 
