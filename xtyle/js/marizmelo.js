bfb84436124e70473ed578763e7437a393cf75e6
Mariz Melo <mm@xchema.com>
2013-01-25 14:43:24 -0800

    removed widgets (not using for now, changed CNAME for website

--- xtyle/js/marizmelo.js
+++ xtyle/js/marizmelo.js
@@ -4,12 +4,12 @@ define(["jquery"], function($){
 
   // FIRST TIME OPENING PAGE
   $('aside img').load(function(){
-    $("#marizlogo").css( { "margin-left" : "80px", "margin-top" : "100px" }).delay(1000).animate( { "margin-left" : "0", "margin-top" : "0" } ); 
+    $("#marizlogo").css( { "margin-left" : "80px", "margin-top" : "85px" }).delay(1000).animate( { "margin-left" : "0", "margin-top" : "0" } ); 
   });
 
   // MOUSE OVER LOGO EFFECT
   $("#marizimage").on("mouseenter", function () {
-    $("#marizlogo").stop().animate( { "margin-left" : "80px", "margin-top" : "100px" }, function() {
+    $("#marizlogo").stop().animate( { "margin-left" : "80px", "margin-top" : "85px" }, function() {
       $(this).on("mouseleave", function(){ 
         $(this).stop().animate( { "margin-left" : "0", "margin-top" : "0" } ); 
       });
