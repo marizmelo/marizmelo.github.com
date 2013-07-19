6cac7e78d0aa877c809a6d4125adaea64cf8cde7
Mariz Melo <mm@xchema.com>
2013-07-19 13:56:41 -0700

    adding files

---
+++ _site/server/xtyle/js/marizmelo.js
@@ -0,0 +1,18 @@
+/*  MARIZMELO.com JavaScript file */
+define(["jquery"], function($){
+
+  // FIRST TIME OPENING PAGE
+  $('aside img').load(function(){
+    $("#marizlogo").css( { "margin-left" : "80px", "margin-top" : "85px" }).delay(1000).animate( { "margin-left" : "0", "margin-top" : "0" } ); 
+  });
+
+  // MOUSE OVER LOGO EFFECT
+  /*$("#marizimage").on("mouseenter", function () {
+    $("#marizlogo").stop().animate( { "margin-left" : "80px", "margin-top" : "85px" }, function() {
+      $(this).on("mouseleave", function(){ 
+        $(this).stop().animate( { "margin-left" : "0", "margin-top" : "0" } ); 
+      });
+    });
+  });*/
+
+});
